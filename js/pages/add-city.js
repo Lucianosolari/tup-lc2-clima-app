330a52ded450a2a5f6c78e36265152aa03df1a5b
Lucianosolari <luciano3924@gmail.com>
2022-07-02 21:24:38 -0300

    Icono cargando

--- js/pages/add-city.js
+++ js/pages/add-city.js
@@ -1,6 +1,12 @@
 let botonAgregar = document.getElementById("agregar");
 botonAgregar.addEventListener("click", addCityToLocalStorage)
 
+let exitoMessage = '<p class="alerta success" >Ciudad agregada con éxito</p>';
+let errorMessage = '<p class="alerta error" >Error: La ciudad ingresada no se encuenta en la API o se produjo un error al consultar</p>';
+let existeMessage = '<p class="alerta warning">La ciudad ingresada ya se encuentra almacenada</p>';
+
+const loader = document.querySelector(".loader");
+
 async function validateCity(newCity) {
     let cities = getCitiesFromLocalStorage();
 
@@ -18,34 +24,35 @@ async function validateCity(newCity) {
 }
 
 function removeMessage() {
-
     setTimeout(function() {
-        document.getElementsByClassName("alerta")[0].remove();
-    }, 3000);
+        remov = document.getElementsByClassName("alerta");
+        remov[0].remove();
+    }, 4000);
 }
 
 async function addCityToLocalStorage() {
     let cities = getCitiesFromLocalStorage();
     let newCity = document.getElementById("ciudad_agregada").value;
 
+    loader.style.display = 'block';
+
     switch (await validateCity(newCity)) {
         case "success":
+            loader.style.display = 'none';
             cities.push(newCity);
             localStorage.setItem("CITIES", JSON.stringify(cities));
             document.getElementById("añadir_ciudad").innerHTML += exitoMessage;
             removeMessage();
             break;
         case "warning":
+            loader.style.display = 'none';
             document.getElementById("añadir_ciudad").innerHTML += existeMessage
             removeMessage();
             break;
         case "error":
+            loader.style.display = 'none';
             document.getElementById("añadir_ciudad").innerHTML += errorMessage;
             removeMessage();
             break;
     };
 };
-
-let exitoMessage = '<p class="alerta success" >Ciudad agregada con éxito</p>';
-let errorMessage = '<p class="alerta error" >Error: La ciudad ingresada no se encuenta en la API o se produjo un error al consultar</p>';
-let existeMessage = '<p class="alerta warning">La ciudad ingresada ya se encuentra almacenada</p>';
